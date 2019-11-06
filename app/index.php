0817664f3598f6330c246075f5bd56c2c7490048
danieldebrito <danieldebrito@outlook.com>
2019-11-06 08:54:26 -0300

    cors

--- app/index.php
+++ app/index.php
@@ -81,10 +81,42 @@ $app->group('/MaqRto', function () {
 $app->add(function ($req, $res, $next) {
   $response = $next($req, $res);
   return $response
+    ->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+
+$app->run();
+
+/*
+
+$app->add(function ($req, $res, $next) {
+  $response = $next($req, $res);
+  return $response
+    ->withHeader('Access-Control-Allow-Origin', 'http://danieldebrito.com.ar')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+*/
+
+/*
+
+$app->add(function ($req, $res, $next) {
+  $response = $next($req, $res);
+  return $response
+  ->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+  ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+$app->add(function ($req, $res, $next) {
+  $response = $next($req, $res);
+  return $response
           ->withHeader('Access-Control-Allow-Origin', 'http://juntasmeyro.com.ar')
           ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
           ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
 
-
-$app->run();
+*/
